41c08e08b0fda56bfe23d01c74ec955304f74c18
questzz <questzz@naver.com>
2022-06-15 15:32:17 +0900

    init project

---
+++ tecoblog/src/main/java/com/tecoding/blog/TecoblogApplication.java
@@ -0,0 +1,13 @@
+package com.tecoding.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecoblogApplication.class, args);
+	}
+
+}
---
+++ tecoblog/src/test/java/com/tecoding/blog/TecoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecoding.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
